34229914980032bd099fabdf50e157c390dddd4b
Noverish Harold <embrapers263@gmail.com>
2018-01-25 19:45:16 +0900

    reformat archive.js

--- routes/archive.js
+++ routes/archive.js
@@ -8,97 +8,92 @@ const s3 = new AWS.S3();
 
 const bucketName = 'noverish';
 
-function getObjectList(path) {
-    return new Promise(function (resolve, reject) {
-        const params = {
-            Bucket: bucketName,
-            Prefix: path,
-            Delimiter: '/'
-        };
-
-        s3.listObjects(params, function (err, data) {
-            if (err) {
-                reject(err);
-                return
-            }
-
-            let objects = [];
-
-            data.CommonPrefixes.forEach(function(element) {
-                let link = '/archive/' + element.Prefix;
-                let name = element.Prefix.replace(path, '');
-                objects.push({
-                    link: link,
-                    name: name
-                })
-            });
-            data.Contents.forEach(function(element) {
-                let link = '/archive/' + element.Key;
-                let name = element.Key.replace(path, '');
-                objects.push({
-                    link: link,
-                    name: name
-                })
-            });
-
-            resolve(objects);
-        });
-    });
-}
-
-function getObjectData(key) {
-    return new Promise(function (resolve, reject) {
-        const params = {
-            Bucket: bucketName,
-            Key: key
-        };
-
-        s3.getObject(params, function(err, data) {
-            if (err) {
-                reject(err);
-                return
-            }
-
-            resolve(data);
-        });
-    });
-}
-
-/* GET home page. */
 router.get('/', function(req, res, next) {
-    const path = '';
-
-    getObjectList(path)
-        .then((objects) => {
-            res.render('archive', { path: path, objects: objects });
-        })
-        .catch((err) => {
-            res.json(err);
-        });
+    checkObjectExist('/', req, res);
 });
 
-/* GET home page. */
 router.get('/:path*', function(req, res, next) {
-    const path = req.param('path') + req.param(0);
-
-    getObjectData(path)
-        .then(data => {
-            res.end(data.Body);
-        })
-        .catch(err => {
-            getObjectList(path + '/')
-                .then(objects => {
-                    objects.unshift({
-                        link: '../',
-                        name: '../'
-                    });
-
-                    res.render('archive', { path: path, objects: objects });
-                })
-                .catch(err => {
-                    res.json(err);
-                })
-        });
+    const path = req.params.path + req.params['0'];
+    checkObjectExist(path, req, res);
 });
+
+function checkObjectExist(path, req, res) {
+    const params = {
+        Bucket: bucketName,
+        Key: path
+    };
+
+    s3.headObject(params, function(err, data) {
+        if(err) {
+            showObjectList(path, req, res);
+        } else {
+            showObject(path, req, res);
+        }
+    });
+}
+
+function showObject(path, req, res) {
+    const params = {
+        Bucket: bucketName,
+        Key: path
+    };
+
+    s3.getObject(params, function(err, data) {
+        if (err) {
+            res.end(JSON.stringify(err));
+        } else {
+            res.end(data.Body);
+        }
+    });
+}
+
+function showObjectList(path, req, res) {
+    if(path === '/')
+        path = '';
+    else
+        path += '/';
+
+    const params = {
+        Bucket: bucketName,
+        Prefix: path,
+        Delimiter: '/'
+    };
+
+    s3.listObjects(params, function (err, data) {
+        if (err) {
+            res.end(JSON.stringify(err));
+            return;
+        }
+
+        let objects = [];
+
+        data.CommonPrefixes.forEach(function(element) {
+            let link = '/archive/' + element.Prefix;
+            let name = element.Prefix.replace(path, '');
+            objects.push({
+                link: link,
+                name: name
+            })
+        });
+
+        data.Contents.forEach(function(element) {
+            let link = '/archive/' + element.Key;
+            let name = element.Key.replace(path, '');
+            objects.push({
+                link: link,
+                name: name
+            })
+        });
+
+        if(path !== '/') {
+            objects.unshift({
+                link: '../',
+                name: '../'
+            });
+        }
+
+        res.render('archive', { path: path, objects: objects });
+    });
+}
 
 module.exports = router;
